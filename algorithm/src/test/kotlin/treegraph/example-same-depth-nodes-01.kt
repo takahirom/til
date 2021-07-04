cec40d547c0782e516b6f104e9c8a62844413d7c
takahirom <takam.dev@gmail.com>
2021-07-04 18:31:34 +0900

    Add tree algorithms

--- algorithm/src/test/kotlin/treegraph/example-same-depth-nodes-01.kt
+++ algorithm/src/test/kotlin/treegraph/example-same-depth-nodes-01.kt
@@ -6,5 +6,5 @@ import com.github.takahirom.tree.*
 fun main() {
     val tree = BinarySearchTree.of(listOf(0, 1, 2, 3, 4, 5, 6))
     tree.root.printTree()
-    println(tree.depthToNodes())
+    println(tree.depthNodes())
 }
